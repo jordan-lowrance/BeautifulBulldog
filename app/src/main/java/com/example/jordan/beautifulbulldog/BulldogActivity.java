9d9a51278b5652abfca2652947c467cecc88c366
jordan.lowrance <jordan.lowrance@drake.edu>
2018-10-05 19:56:59 -0500

    Roadblock
       layoutManager object is being considered null for some reason, not sure what chenged to cause this error.
    
     java.lang.NullPointerException: Attempt to invoke virtual method 'void android.support.v7.widget.RecyclerView.setLayoutManager(android.support.v7.widget.RecyclerView$LayoutManager)' on a null object reference
            at com.example.jordan.beautifulbulldog.BulldogListFragment.onCreateView(BulldogListFragment.java:47)

--- app/src/main/java/com/example/jordan/beautifulbulldog/BulldogActivity.java
+++ app/src/main/java/com/example/jordan/beautifulbulldog/BulldogActivity.java
@@ -1,13 +1,23 @@
 package com.example.jordan.beautifulbulldog;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import io.realm.Realm;
 
+import static com.example.jordan.beautifulbulldog.MainActivity.user;
+
 public class BulldogActivity extends AppCompatActivity {
 
+    private Button button;
+    private RatingBar ratingBar;
+    private TextView bulldogAge;
+    private TextView bulldogName;
     private TextView textView;
     private Realm realm;
 
@@ -19,14 +29,36 @@ public class BulldogActivity extends AppCompatActivity {
         textView = findViewById(R.id.textView);
         realm = Realm.getDefaultInstance();
 
+        bulldogAge = findViewById(R.id.bulldogAge);
+        bulldogName = findViewById(R.id.bulldogName);
+        ratingBar = findViewById(R.id.ratingBar);
+
+        button = findViewById(R.id.button);
+
         String id = (String) getIntent().getStringExtra("bulldog");
         Bulldog bulldog = realm.where(Bulldog.class).equalTo("id", id).findFirst();
 
         Realm realm = Realm.getDefaultInstance();
         String username = (String) getIntent().getStringExtra("username");
-        MainActivity.user = realm.where(User.class).equalTo("username", username).findFirst();
-
+        user = realm.where(User.class).equalTo("username", username).findFirst();
 
         textView.setText(bulldog.getName());
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Realm realm = Realm.getDefaultInstance();
+                realm.executeTransaction(new Realm.Transaction() {
+                    @Override
+                    public void execute(Realm realm) {
+                        Vote vote = new Vote();
+                        vote.setRating(ratingBar.getNumStars());
+                        //realm.copyToRealmOrUpdate(vote);
+                        finish();
+                    }
+                });
+            }
+        });
     }
 }
